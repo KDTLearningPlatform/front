a77bee215ff1c0a2572c75af4ce18e032e636928
hansol <waw8805@hanmail.net>
2024-07-21 05:07:18 +0900

    라우팅 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Splash from './pages/splash'; 
-// import Login from './pages/login';
+import Splash from './pages/splash'; 
+import Login from './pages/login';
 import ProfileCompletion from './pages/profilecompletion';
 import MyCourses from './pages/MyCourses';
 import CommunityPage from './pages/CommunityPage';
 import WritePostPage from './pages/WritePostPage'; 
-import TabBar from './components/TabBar/TabBar'; 
+
+
 const App = () => {
-  // const [showSplash, setShowSplash] = useState(true);
-  // const [showLogin, setShowLogin] = useState(false);
-  // const [showProfileCompletion, setShowProfileCompletion] = useState(false);
-  const [showMyCourses, setShowMyCourses] = useState(true);
+  const [showSplash, setShowSplash] = useState(true);
+  const [showLogin, setShowLogin] = useState(false);
+  const [showProfileCompletion, setShowProfileCompletion] = useState(false);
+  const [showMyCourses, setShowMyCourses] = useState(false);
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     setShowSplash(false);
-  //     setShowLogin(true);
-  //   }, 3000); // 3초 후에 스플래시 화면이 사라짐
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+      setShowLogin(true);
+    }, 3000);
 
-  //   return () => clearTimeout(timer); // 컴포넌트가 언마운트될 때 타이머를 정리함
-  // }, []);
+    return () => clearTimeout(timer); 
+  }, []);
 
-  // const handleLoginSuccess = () => {
-  //   setShowLogin(false);
-  //   setShowProfileCompletion(true);
-  // };
+  const handleLoginSuccess = () => {
+    setShowLogin(false);
+    setShowProfileCompletion(true);
+  };
 
-  // const handleProfileCompletionSuccess = () => {
-  //   setShowProfileCompletion(false);
-  //   setShowMyCourses(true);
-  // };
+  const handleProfileCompletionSuccess = () => {
+    setShowProfileCompletion(false);
+    setShowMyCourses(true);
+  };
 
   return (
     <Router>
       <div style={{ paddingBottom: '60px' }}> 
         <Routes>
-          {/* {showSplash && <Splash />} */}
-          {/* {!showSplash && showLogin && <Login onLoginSuccess={handleLoginSuccess} />} */}
-          {/* {!showSplash && !showLogin && showProfileCompletion && <ProfileCompletion onProfileCompletionSuccess={handleProfileCompletionSuccess} />} */}
-          {/* {!showSplash && !showLogin && !showProfileCompletion && showMyCourses && <MyCourses />} */}
-          {showMyCourses && <Route path="/" element={<MyCourses />} />}
+          {showSplash && <Route path="/" element={<Splash />} />}
+          {!showSplash && showLogin && <Route path="/" element={<Login onLoginSuccess={handleLoginSuccess} />} />}
+          {!showSplash && !showLogin && showProfileCompletion && <Route path="/" element={<ProfileCompletion onProfileCompletionSuccess={handleProfileCompletionSuccess} />} />}
+          {!showSplash && !showLogin && !showProfileCompletion && showMyCourses && <Route path="/" element={<MyCourses />} />}
           <Route path="/profile" element={<ProfileCompletion />} />
           <Route path="/courses" element={<MyCourses />} />
           <Route path="/community" element={<CommunityPage />} />
           <Route path="/write-post" element={<WritePostPage />} /> 
         </Routes>
-        <TabBar />
+        
       </div>
     </Router>
   );
